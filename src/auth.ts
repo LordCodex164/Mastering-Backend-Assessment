07ca8835b9852812671a4ff46443ca26e5e6b281
lordcodex dev <adenirandaniel565@gmail.com>
2025-08-01 14:00:53 +0100

    fix signinOtions for oauth

--- src/auth.ts
+++ src/auth.ts
@@ -23,31 +23,48 @@ export const authOptions: NextAuthOptions = {
         strategy: "jwt",
     },
     callbacks:{
-        async signIn({user}){
-            try{
-            let user_ = await prisma.user.findUnique({
-                where: {
-                    email: user?.email!,
+            async signIn({ user, account, profile }: any) {
+              // Only handle OAuth providers
+              if (account.type === 'oauth') {
+                const existingUser = await prisma.user.findUnique({
+                  where: { email: profile.email },
+                  include: { accounts: true },
+                });
+        
+                // If user exists but the provider isn't linked, link the account
+                if (existingUser && !existingUser.accounts.some(acc => acc.provider === account.provider && acc.providerAccountId === account.providerAccountId)) {
+                  await prisma.account.create({
+                    data: {
+                      userId: existingUser.id,
+                      type: account.type,
+                      provider: account.provider,
+                      providerAccountId: account.providerAccountId,
+                      access_token: account.access_token,
+                      refresh_token: account.refresh_token,
+                      expires_at: account.expires_at,
+                      token_type: account.token_type,
+                      scope: account.scope,
+                      id_token: account.id_token,
+                      session_state: account.session_state,
+                      //oauth_token_secret: account.oauth_token_secret!,
+                      //oauth_token: account.oauth_token,
+                    },
+                  });
+                  return true; // Allow sign-in
                 }
-            })
-            if(!user_){
-             user_ = await prisma.user.create({
-                data: {
-                    email: user?.email!,
-                }
-            })
-            }
-            return true;
-            }catch(error){
-                console.log(error, "error")
-                return false;
-            }
-        },
+              }
+              return true; // Proceed with default behavior
+            },
         async session({session, token}){
             try{
                 if(token){
                     console.log(token, "token")
+                    console.log(session, "session")
+                    //session.user!.id = token.sub!;
+                } else {
+                    //session.user!.id = token.sub!; 
                 }
+                return session;
             }catch(error){
                 console.log(error, "error")
             }
