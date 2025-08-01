63fb55e1ae7ef7b5cd4095680041229f3f38ff09
lordcodex dev <adenirandaniel565@gmail.com>
2025-08-01 14:19:21 +0100

    redirect user to sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,10 @@ export default async function Home() {
 
   const session = await getServerSession(authOptions);
 
+  if(!session){
+    redirect("/sign-in")
+  }
+
   const cards = [
     {
       icon: "∞",
